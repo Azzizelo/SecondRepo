75453778ca45bff46821536f63505b8ac90ca7b7
Azzizelo <S00271367@atu.ie>
2025-02-06 14:30:13 +0000

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,8 +1,11 @@
 // the setup routine runs once when you press reset:
-void setup() {
+int LED = 2;void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  
+  pinMode(LED), OUTPUT);
 }
+
 
 // the loop routine runs over and over again forever:
 void loop() {
